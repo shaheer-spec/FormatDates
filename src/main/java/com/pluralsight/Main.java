6b11e2247d03a4f6954d7a714be2e2cdbda3e54a
Shaheer kayani <shaheer.kayani8@gmail.com>
2025-10-07 14:17:26 -0400

    Initial Commit and push with code

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,4 +1,28 @@
 package com.pluralsight;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Main {
+    public static void main(String[] args) {
+
+        LocalDate date = LocalDate.now();
+
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+        System.out.println(date.format(dateTimeFormatter));
+
+        DateTimeFormatter dateTimeFormatter2 = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        System.out.println(date.format(dateTimeFormatter2));
+
+        DateTimeFormatter dateTimeFormatter3 = DateTimeFormatter.ofPattern("MMMM d, yyyy");
+        System.out.println(date.format(dateTimeFormatter3));
+
+        LocalDateTime dateTime = LocalDateTime.now();
+        DateTimeFormatter dateTimeFormatter4 = DateTimeFormatter.ofPattern("EEEE, MMM d, yyyy HH:mm");
+        String formatedDate4 = dateTime.format(dateTimeFormatter4);
+        System.out.println(formatedDate4);
+        // Initial attempt without GMT time
+
+    }
 }
